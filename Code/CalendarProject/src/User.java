f22ce5598819c6963d41b02fafe5b2035c1bb5bf
Daniella1818 <113984678+Daniella1818@users.noreply.github.com>
2023-05-13 09:05:58 +0200

    User tábla

---
+++ Code/CalendarProject/src/User.java
@@ -0,0 +1,8 @@
+public class User {
+    private int ID;
+    private String Username;
+    private String Firstname;
+    private String Lastname;
+    private String Email;
+    private String Password;
+}
