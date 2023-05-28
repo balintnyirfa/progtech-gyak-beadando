ed38c82862d987939590019d4ec4dcccd6802103
Daniella1818 <113984678+Daniella1818@users.noreply.github.com>
2023-05-28 20:34:57 +0200

    Add event

--- Code/CalendarProject/src/Pages/AddEventPage.java
+++ Code/CalendarProject/src/Pages/AddEventPage.java
@@ -1,9 +1,19 @@
 package Pages;
 
+import classes.User;
+import classes.Event;
+import Calendar.CalendarAbstract;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.Calendar;
+import java.util.Date;
 
 public class AddEventPage extends JDialog{
     private JTextField eventTitle;
@@ -13,6 +23,9 @@ public class AddEventPage extends JDialog{
     private JButton eventAdd;
     private JPanel createEvent;
     private JButton visszaButton;
+
+    //CALENDAR kiválasztástól!
+    private CalendarAbstract calendar;
 
     public AddEventPage(JFrame parent){
         super(parent);
@@ -26,7 +39,7 @@ public class AddEventPage extends JDialog{
         eventAdd.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                AddEventToDatabase();
             }
         });
         visszaButton.addActionListener(new ActionListener() {
@@ -38,5 +51,50 @@ public class AddEventPage extends JDialog{
             }
         });
         setVisible(true);
+    }
+
+    private void AddEventToDatabase(){
+        Event event = null;
+        String title = eventTitle.getText();
+        String content = eventContent.getText();
+        //int calendar.ID;
+        //Date from;
+        //Dare to;
+
+        if(title.isEmpty() || content.isEmpty()){
+            JOptionPane.showMessageDialog(this, "Az összes mezőt ki kell tölteni!", "Error", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+
+        final String DB_URL = "jdbc:mysql://localhost/calendar?serverTimezone=UTC";
+        final String USERNAME = "root";
+        final String PASSWORD = "";
+
+        try {
+            Connection conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+            Statement stm = conn.createStatement();
+            String sql = "INSERT INTO user(title, content, calendar_id, from, to) VALUES (?,?,?,?, ?)";
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            preparedStatement.setString(1, title);
+            preparedStatement.setString(2, content);
+            //preparedStatement.setString(3, calendar_id);
+            //preparedStatement.setString(4, lastname);
+            //preparedStatement.setString(5, password);
+
+            int addedRows = preparedStatement.executeUpdate();
+            if (addedRows > 0) {
+                event = new Event();
+                event.setTitle(title);
+                event.setContent(content);
+                //event.setFrom(from);
+                //event.setTo(to);
+                calendar.addEvent(event);
+            }
+            stm.close();
+            conn.close();
+        }
+        catch (Exception exception){
+            System.out.println(exception.getMessage());
+        }
     }
 }
