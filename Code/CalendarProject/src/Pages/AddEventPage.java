38047707bc97fb598400525b9a8bcd60ef021edc
Daniella1818 <113984678+Daniella1818@users.noreply.github.com>
2023-06-02 11:06:00 +0200

    Esemény hozzáadása működik

--- Code/CalendarProject/src/Pages/AddEventPage.java
+++ Code/CalendarProject/src/Pages/AddEventPage.java
@@ -16,20 +16,21 @@ import java.sql.Statement;
 import java.util.Calendar;
 
 public class AddEventPage extends JDialog{
+
+    private JPanel createEvent;
     private JTextField eventTitle;
     private JTextField eventContent;
     private JButton eventAdd;
-    private JPanel createEvent;
-    private JButton visszaButton;
+    private JButton backButton;
     private JPanel startDateJP;
     private JPanel endDateJP;
 
     //CALENDAR kiválasztástól!
     private CalendarAbstract calendar;
 
-    Calendar cld = Calendar.getInstance(); //Aktuális dátum
-    JDateChooser startDateChooser2 = new JDateChooser(cld.getTime()); //Dátum választó mai dátummal kezdőértékként.
-    JDateChooser endDateChooser2 = new JDateChooser(cld.getTime());
+    Calendar cld = Calendar.getInstance();
+    JDateChooser startDateChooser = new JDateChooser(cld.getTime());
+    JDateChooser endDateChooser = new JDateChooser(cld.getTime());
 
     public AddEventPage(JFrame parent){
         super(parent);
@@ -39,13 +40,8 @@ public class AddEventPage extends JDialog{
         setModal(true);
         setLocationRelativeTo(parent);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-        try {
-            startDateJP.add(startDateChooser2);
-            endDateJP.add(endDateChooser2);
-        }
-        catch (Exception exception){
-            System.out.println(exception.getMessage());
-        }
+        startDateJP.add(startDateChooser);
+        endDateJP.add(endDateChooser);
 
         eventAdd.addActionListener(new ActionListener() {
             @Override
@@ -53,7 +49,7 @@ public class AddEventPage extends JDialog{
                 AddEventToDatabase();
             }
         });
-        visszaButton.addActionListener(new ActionListener() {
+        backButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 dispose();
@@ -69,12 +65,13 @@ public class AddEventPage extends JDialog{
         String title = eventTitle.getText();
         String content = eventContent.getText();
 
-        java.util.Date startDate = startDateChooser2.getDate();
+        java.util.Date startDate = startDateChooser.getDate();
         java.sql.Date from = new java.sql.Date(startDate.getTime());
 
-        java.util.Date endDate = endDateChooser2.getDate();
+        java.util.Date endDate = endDateChooser.getDate();
         java.sql.Date to = new java.sql.Date(endDate.getTime());
         //int calendar.ID;
+        int id = 1;
 
         if(title.isEmpty() || content.isEmpty()){
             JOptionPane.showMessageDialog(this, "Az összes mezőt ki kell tölteni!", "Error", JOptionPane.ERROR_MESSAGE);
@@ -88,11 +85,11 @@ public class AddEventPage extends JDialog{
         try {
             Connection conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
             Statement stm = conn.createStatement();
-            String sql = "INSERT INTO user(title, content, calendar_id, from, to) VALUES (?,?,?,?, ?)";
+            String sql = "INSERT INTO event(title, content, calendar_id, from_date, to_date) VALUES (?,?,?,?, ?)";
             PreparedStatement preparedStatement = conn.prepareStatement(sql);
             preparedStatement.setString(1, title);
             preparedStatement.setString(2, content);
-            //preparedStatement.setString(3, cal_id);
+            preparedStatement.setInt(3, id);
             preparedStatement.setDate(4, from);
             preparedStatement.setDate(5, to);
 
@@ -101,10 +98,10 @@ public class AddEventPage extends JDialog{
                 event = new Event();
                 event.setTitle(title);
                 event.setContent(content);
-                //event.setFrom(from);
-                //event.setTo(to);
-                AddEventCommand a = new AddEventCommand(calendar, event);
-                a.ExecuteEvent();
+                event.setFrom(from);
+                event.setTo(to);
+                //AddEventCommand a = new AddEventCommand(calendar, event);
+                //a.ExecuteEvent();
             }
             stm.close();
             conn.close();
